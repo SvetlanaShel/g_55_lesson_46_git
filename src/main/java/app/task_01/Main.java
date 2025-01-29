d8d5ee8d1ccd12b616f3903c18624956646bd065
SvetlanaShel <sveta61shel169@gmail.com>
2025-01-29 09:55:43 +0100

    Task 1

---
+++ src/main/java/app/task_01/Main.java
@@ -0,0 +1,11 @@
+package app.task_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Этот код, который решает задачу 1
+        System.out.println("Task 1");
+    }
+}
+
