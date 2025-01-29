e046a43814aee60828e8048f0619c47579127b11
SvetlanaShel <sveta61shel169@gmail.com>
2025-01-29 10:39:23 +0100

    Task 2

---
+++ src/main/java/app/task_02/Main.java
@@ -0,0 +1,11 @@
+package app.task_02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Новый функционал задачи 2
+        System.out.println("Task 2");
+    }
+}
+
